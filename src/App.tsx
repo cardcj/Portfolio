bf08c13d1012f58e464b88e0d6392f6272ccce03
cardcj <cardwell.jamesc@gmail.com>
2021-09-03 19:03:18 +0100

    Changed imports to use React from react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './Theme';
--- src/components/text/Heading.tsx
+++ src/components/text/Heading.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { spacing } from '../../Theme';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';

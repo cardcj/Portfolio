f6865237c84f64b7223b0d13f2e5bf69092b68ce
cardcj <cardwell.jamesc@gmail.com>
2021-09-03 19:55:00 +0100

    Fixed variable misnomer in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { spacing } from '../Theme';
 
-const Footer = styled.footer`
+const StyledFooter = styled.footer`
   background-color: ${(props) => props.theme.colors.common.grey};
   position: fixed;
   bottom: 0;
@@ -14,8 +14,8 @@ const Footer = styled.footer`
   box-shadow: 0 -1px 2px 1px rgba(0, 0, 0, 0.1);
 `;
 
-const Header = (): React.ReactElement => {
-  return <Footer />;
+const Footer = (): React.ReactElement => {
+  return <StyledFooter />;
 };
 
-export default Header;
+export default Footer;

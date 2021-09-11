83db4b7b5848b21da6c03547faac36a01bbbf5ea
cardcj <cardwell.jamesc@gmail.com>
2021-09-11 15:11:09 +0100

    Added background to the NavButton component so it can be seen when viewed on its own

--- src/components/input/NavButton.tsx
+++ src/components/input/NavButton.tsx
@@ -12,13 +12,12 @@ const ButtonWrapper = styled.div`
 `;
 
 const NavButtonStyled = styled.button`
-  background-color: transparent;
+  background-color: ${(props) => props.theme.colors.common.grey};
   min-width: 100px;
   height: 40px;
   font-size: 1em;
   border: none;
   padding: 0 ${spacing(4)};
-  border-radius: ${radiusConstraint(4)};
   font-family: ${(props) => props.theme.fonts.content};
   color: ${(props) => props.theme.colors.common.white};
 
